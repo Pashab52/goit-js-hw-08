e40fc0fd53412c2896bbd18ea3c7de07a6aea1d4
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-05-13 00:14:08 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,34 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>`;
+  })
+  .join('');
+
+const galleryList = document.querySelector('.gallery');
+
+galleryList.innerHTML = galleryMarkup;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  animationSpeed: 150,
+  fadeSpeed: 150,
+  animationSlide: false,
+  showCounter: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+  disableScroll: false,
+});
+
+console.log(gallery);
