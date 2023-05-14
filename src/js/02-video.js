359ed10372de2d6925e4a23adfcebbb42f52a0cc
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-05-14 20:10:45 +0300

    t2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,33 @@
 import Player from '@vimeo/player';
 
-const player = new Player('handstick', {
-  id: 19231868,
-  width: 640,
-});
+var throttle = require('lodash.throttle');
+
+const player = new Player('vimeo-player');
+
+function playerCurrentTimeRestore() {
+  const onPlay = function ({ seconds }) {
+    localStorage.setItem('videoplayer - current - time', `${seconds}`);
+    console.log(localStorage.getItem('videoplayer - current - time'));
+  };
+
+  player.on('timeupdate', throttle(onPlay, 1000));
+
+  player
+    .setCurrentTime(localStorage.getItem('videoplayer - current - time'))
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
+
+playerCurrentTimeRestore();
