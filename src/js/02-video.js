4e060bb36428b7623bf824fe4aadaab19b8fb556
Pashab52 <120509362+Pashab52@users.noreply.github.com>
2023-05-15 16:54:20 +0300

    t3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -12,22 +12,28 @@ function playerCurrentTimeRestore() {
 
   player.on('timeupdate', throttle(onPlay, 1000));
 
-  player
-    .setCurrentTime(localStorage.getItem('videoplayer - current - time'))
-    .then(function (seconds) {
-      // seconds = the actual time that the player seeked to
-    })
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
+  const playerCurrentTime = localStorage.getItem(
+    'videoplayer - current - time'
+  );
+  if (playerCurrentTime) {
+    // Зробив тут перевірку, але не дуже розумію навіщо воно тут, без неї теж все працює.
+    player
+      .setCurrentTime(playerCurrentTime)
+      .then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+      })
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
 
-        default:
-          // some other error occurred
-          break;
-      }
-    });
+          default:
+            // some other error occurred
+            break;
+        }
+      });
+  }
 }
 
 playerCurrentTimeRestore();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+const formBtn = document.querySelector('.feedback-form button');
+// console.log(formBtn);
+
+const formData = { email: '', message: '' };
+
+restoreFormData();
+function restoreFormData() {
+  const restoreFormData = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  if (restoreFormData) {
+    console.log(restoreFormData);
+    form.email.value = restoreFormData.email;
+    form.message.value = restoreFormData.message;
+    formData.email = restoreFormData.email;
+    formData.message = restoreFormData.email;
+  }
+  btnDisabled();
+}
+
+form.addEventListener('submit', handlerFormSubmit);
+
+function handlerFormSubmit(event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  form.email.value = '';
+  form.message.value = '';
+  btnDisabled();
+}
+
+form.addEventListener('input', throttle(handlerFormInputData, 500));
+
+function handlerFormInputData(event) {
+  btnDisabled();
+  // console.log(event.target.name);
+  // console.log(event.target.value);
+
+  //   console.log(event.currentTarget.elements.email.value);
+  //   console.log(event.currentTarget.elements.message.value);
+
+  formData[event.target.name] = event.target.value;
+  console.log(event.target.value);
+  console.log(formData);
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function btnDisabled() {
+  if (form.email.value !== '' && form.message.value !== '') {
+    formBtn.disabled = false;
+  } else {
+    formBtn.disabled = true;
+  }
+}
